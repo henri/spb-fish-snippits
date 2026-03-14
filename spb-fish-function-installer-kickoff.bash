cce4d981487c09aa95d91923dac87a1d5cc52f13
henri <github.shustak@neverbox.com>
2026-03-15 08:08:04 +1300

    Update spb-fish-function-installer-kickoff.bash

--- spb-fish-function-installer-kickoff.bash
+++ spb-fish-function-installer-kickoff.bash
@@ -1,4 +1,4 @@
-/csse/scratch/juliet/laptop-home-2026-02-10# copy and paste the lines below into a bash shell to setup the spb fish functions listed in this gist.
+# copy and paste the lines below into a bash shell to setup the spb fish functions listed in this gist.
 
 /bin/bash -c "$(curl -fsSL \
 https://gist.githubusercontent.com/henri/\
