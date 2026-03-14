be4dc6b7d231f152a33009b4e3bb5eb46f5bf26e
henri <github.shustak@neverbox.com>
2026-03-15 08:52:12 +1300

    Update spb-fish-function-installer-kickoff.bash

--- spb-fish-function-installer-kickoff.bash
+++ spb-fish-function-installer-kickoff.bash
@@ -1,8 +1,8 @@
 # copy and paste the lines below into a bash shell to setup the spb fish functions listed in this gist.
 
 /bin/bash -c "$(curl -fsSL \
-https://githubusercontent.com/henri/\
-spb-fish-snippits/raw/\
+https://raw.githubusercontent.com/henri/\
+refs/heads/main/\
 spb-fish-function-installer.bash)"
 
 # note this is until a fisher setup is established. 
