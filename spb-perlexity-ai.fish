23c81771d5fc9c94744d9dd55a904523e8efc545
henri <github.shustak@neverbox.com>
2026-03-31 09:05:31 +1300

    Update spb-perlexity-ai.fish

--- spb-perlexity-ai.fish
+++ spb-perlexity-ai.fish
@@ -19,7 +19,7 @@ function spb-pai
                     echo "$argv[1]"
                     echo "$argv[2]"
                     set --erase argv[1]
-                case '--help' '--list' '-ls' '-l' '--list-templates' '--update' '--tor' '--list-browsers' '--configuration-variables'
+                case '--help' '--list' '-ls' '-l' '--list-templates' '--update' '--tor' '--list-browsers' '--configuration-variables' '--version'
                     echo "$argv[1]"
                     break
                 case  '--standard' '--quite' '--quiet' '--verbose' '--about'
