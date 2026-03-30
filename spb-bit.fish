778f26c2028be2acabb02fc96666c750a708c99a
henri <github.shustak@neverbox.com>
2026-03-30 18:04:56 +1300

    Update spb-bit.fish

--- spb-bit.fish
+++ spb-bit.fish
@@ -18,7 +18,7 @@ function spb-bit
                 case '--help' '--list' '-ls' '-l' '--list-templates' '--update' '--tor' '--list-browsers'
                     echo "$argv[1]"
                     break
-                case  '--standard' '--quite' '--verbose' '--about'
+                case  '--standard' '--quite' '--quiet' '--verbose' '--about'
                     echo "$argv[1]"
                 case '*'
                     echo (echo "https://www.bitchute.com/search?query=$argv[1]" | tr " " "+")
