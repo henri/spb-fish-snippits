0f568e162b5600c9410ffdc4c68a52ddedbf4752
henri <github.shustak@neverbox.com>
2026-04-16 19:58:58 +1200

    Create spb-wikipedia.fish

---
+++ spb-wikipedia.fish
@@ -0,0 +1,42 @@
+# one line command to configure spb-wikipedia functioin to start multiple Wikipedia searches in multiple private browser tabs.
+# this function requires spb (start-private-browser) alias to be configured : https://github.com/henri/spb
+# this function requires an alias 'start-private-browser' and 'spb' to be setup eg :
+# alias --save start-private-browser="~/bin/start-private-browser.bash"
+# alias --save spb="start-private-browser"
+
+# run the following in a fish shell to configure for your shell : 
+function spb-wikipedia
+    set url_count 0
+    set urls ( 
+        while test (count $argv) -gt 0
+            switch $argv[1]
+                case '--template' '--new-template' '--edit-template' '--force-stop' '--browser' '--template-path' '--browser-path'
+                    echo "$argv[1]"
+                    echo "$argv[2]"
+                    set --erase argv[1]
+                case '--help' '--list' '-ls' '-l' '--list-templates' '--update' '--tor' '--list-browsers' '--configuration-variables' '--version'
+                    echo "$argv[1]"
+                    break
+                case  '--standard' '--quite' '--quiet' '--verbose' '--about'
+                    echo "$argv[1]"
+                case '*'
+                    echo (echo "https://en.wikipedia.org/w/index.php?search=$argv[1]" | tr " " "+")
+                    set url_count (math $url_count + 1)
+            end
+            set --erase argv[1]
+        end 
+        if test $url_count -eq 0
+            echo "https://gemini.google.com/"
+        end
+    )
+   spb $urls
+end 
+funcsave spb-wikipedia
+
+
+
+# usage example - lets say you want to start five searches in five tabs. Use the command below : 
+# spb-wikipedia "cats creap" "dogs dance" "mice play" "dear dash" "rabbits rule"
+
+# note : this command offers basic support for special characters. The only subsition is space characters are turned into plus signs. If you are searching
+#        for something with odd characters then probably using this script is not a great idea.
